ab1d34252a3d4eefd9350bdb52690e451ec12b5c
Cristian Díaz <cristian@210x7.io>
2025-11-06 15:47:05 +0100

    Fix platform version syntax for Swift 6.1

--- Packages/RealityKitContent/Package.swift
+++ Packages/RealityKitContent/Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "RealityKitContent",
     platforms: [
-        .visionOS(.v26),
-        .macOS(.v26),
-        .iOS(.v26),
-        .tvOS(.v26)
+        .visionOS("26.0"),
+        .macOS("26.0"),
+        .iOS("26.0"),
+        .tvOS("26.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
