f5871f18de05dbe68538ae6e25a6890e68d8a732
Cristian Díaz <cristian@210x7.io>
2025-11-06 13:47:47 +0100

    Initial Commit

---
+++ CompilingRealitySample/CompilingRealitySampleApp.swift
@@ -0,0 +1,40 @@
+//
+//  CompilingRealitySampleApp.swift
+//  CompilingRealitySample
+//
+//  Created by Cristian Díaz on 06.11.25.
+//
+
+import SwiftUI
+
+@main
+struct CompilingRealitySampleApp: App {
+
+    @State private var appModel = AppModel()
+    @State private var avPlayerViewModel = AVPlayerViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            if avPlayerViewModel.isPlaying {
+                AVPlayerView(viewModel: avPlayerViewModel)
+            } else {
+                ContentView()
+                    .environment(appModel)
+            }
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                    avPlayerViewModel.play()
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                    avPlayerViewModel.reset()
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
